919e16b74ce704d8ca31a63ee258dc8c6a0f522f
Lucas Leão <lucasmelloleao@gmail.com>
2023-01-04 10:26:56 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import Image from 'next/image';
+import styles from '../../styles/home.module.scss';
+
+import logoImg from '../../public/logo.svg';
+
+import { Input } from '../components/ui/Input'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>SujeitoPizza - Faça seu login</title> 
+    </Head>
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
+
+      <div className={styles.login}>
+        <form>
+          <Input
+            placeholder="Digite seu email"
+            type="text"
+          />
+
+          <Input
+            placeholder="Sua senha"
+            type="password"
+          />
+
+        </form>
+      </div>
+    </div>
+    </>
+  )
+}
